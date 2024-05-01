5341f7439f89f9a2e0b03f1cd1bf876457e7b62a
Matielo J Geronimo <matielojg@gmail.com>
2024-05-01 19:45:08 -0300

    feat: add product list and product item components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import ProductList from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 const Home = () => {
   return (
@@ -9,10 +12,12 @@ const Home = () => {
       <Header />
       <div className="px-5 pt-6">
         <Search />
-      </div>;
+      </div>
+
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
+
       <div className="px-5 pt-6">
         <Image
           src="/promo-banner-01.png"
@@ -24,7 +29,22 @@ const Home = () => {
           quality={100}
         />
       </div>
-    </>)
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
+      </div>
+    </>
+  );
 };
 
 export default Home;
