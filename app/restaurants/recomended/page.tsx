1c725d2a8b63e9d68513086ec4f73ca49ea5fda7
Matielo J Geronimo <matielojg@gmail.com>
2024-05-03 00:53:11 -0300

    feat: add recommended products page

--- app/restaurants/recomended/page.tsx
+++ app/restaurants/recomended/page.tsx
@@ -1,23 +1,36 @@
 import Header from "@/app/_components/header";
-import RestaurantItem from "@/app/_components/restaurant-item";
+import ProductItem from "@/app/_components/product-item";
 import { db } from "@/app/_lib/prisma";
 
-const RecommendedRestaurants = async () => {
-  const restaurants = await db.restaurant.findMany({});
+const RecommendedProductsPage = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 20,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
 
+  // TODO: pegar produtos com mais pedidos
   return (
     <>
       <Header />
       <div className="px-5 py-6">
-        <h2 className="mb-6 text-lg font-semibold">
-          Restaurantes Recomendados
-        </h2>
-        <div className="flex w-full flex-col gap-6">
-          {restaurants.map((restaurant) => (
-            <RestaurantItem
-              key={restaurant.id}
-              restaurant={restaurant}
-              className="min-w-full max-w-full"
+        <h2 className="mb-6 text-lg font-semibold">Pedidos Recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
             />
           ))}
         </div>
@@ -26,4 +39,4 @@ const RecommendedRestaurants = async () => {
   );
 };
 
-export default RecommendedRestaurants;
+export default RecommendedProductsPage;
